83099afecd95732f6bb13ba5677c620592dc3354
Emmanuel Zheng <emmanuelzheng@Emmanuels-MBP.twcny.rr.com>
2020-12-01 10:53:48 -0500

    Add posting, and liking functionality, ability to click into a post to leave comments and see other comments

---
+++ client/src/components/posts/PostForm.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addPost } from '../../actions/post';
+
+const PostForm = ({
+  addPost,
+  auth: {
+    user: { name },
+  },
+}) => {
+  // just going be be a string since this is just a single string
+  const [text, setText] = useState('');
+
+  return (
+    <div className='post-form'>
+      <div className='bg-primary p'>
+        <h3>Say Something...</h3>
+      </div>
+      <form
+        className='form my-1'
+        onSubmit={(e) => {
+          e.preventDefault();
+          addPost({ text });
+          setText('');
+        }}
+      >
+        <textarea
+          name='text'
+          cols='30'
+          rows='5'
+          placeholder={`What's on your mind, ${name.split(' ')[0]}?`}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          required
+        ></textarea>
+        <input type='submit' className='btn btn-dark my-1' value='Submit' />
+      </form>
+    </div>
+  );
+};
+
+PostForm.propTypes = {
+  addPost: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { addPost })(PostForm);
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,10 +3,12 @@ import { combineReducers } from 'redux';
 import alert from './alert';
 import auth from './auth';
 import profile from './profile';
+import post from './post';
 
 //takes in object that has any reducers we create
 export default combineReducers({
   alert,
   auth,
   profile,
+  post,
 });
